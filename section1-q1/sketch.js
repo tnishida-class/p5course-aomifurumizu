7c0edad5ef211915f62e34f2a033f38cf0e0c76e
aomifurumizu <92346424+aomifurumizu@users.noreply.github.com>
2021-10-19 11:46:04 +0900

    section1-q

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -1,18 +1,30 @@
 // 練習問題：神戸市のマーク
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
+  stroke(22, 131, 46)
   strokeWeight(25);
   strokeCap(SQUARE);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
+ // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+
+  fill(blue);
+  noStroke();
+  ellipse(300, 100, 100, 100);
+  fill(255);
+  ellipse(295, 95, 88, 88);
+  fill(green);
+  ellipse(290, 90, 75, 75);
+  fill(255);
+  ellipse(286, 96, 67, 67);
 }
