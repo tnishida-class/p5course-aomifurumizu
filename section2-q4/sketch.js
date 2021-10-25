4618fe8632d09eab56a10f0e1b0cbd02becadc06
aomifurumizu <92346424+aomifurumizu@users.noreply.github.com>
2021-10-26 01:41:27 +0900

    section2-q4, Greece

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -10,12 +10,25 @@ function setup() {
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
+    if(i%2==0){
+      fill(255)
+    }
+    else{
+      fill(blue)
+    }
   }
 
+  noStroke();
   fill(blue);
   let size = d * 5;
   rect(0, 0, size, size);
 
   fill(255);
+  rect(0,2*d,size,d)
+  noStroke()
+  rect(30,0,d,size)
+
+  fill(blue)
+  rect(size,0, width,d)
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
