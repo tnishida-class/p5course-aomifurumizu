df4657f5e75810e581e2e458945302e397a18f02
aomifurumizu <92346424+aomifurumizu@users.noreply.github.com>
2021-10-31 13:04:56 +0900

    section3-q1  2090523h

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -6,11 +6,27 @@ function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
-}
+  }
+
+  function mousePressed(){
+    cycle=50;
+    }
+
+  function mouseReleased(){
+    cycle=100;
+    }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
+
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  //let size=count;　これでy=xみたいになる
+if(count>cycle/2){
+  ellipse(width/2,height/2,count);
+  }
+  else{
+    ellipse(width/2,height/2,cycle-count)
+  }
 }
+  //ellipse(width / 2, height / 2, size);
